80dc9c04226f467642dc91fd25e56cde4d664a6d
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-14 00:06:59 +0300

    Create Timer.cs

---
+++ Timer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int timer= 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer++;
+            label1.Text = timer.ToString();
+            if(timer == 5)
+            {
+                this.BackColor = Color.BlueViolet;
+            }
+            if(timer == 10)
+            {
+                this.BackColor = Color.OrangeRed;
+            }
+            if(timer == 15)
+            {
+                this.BackColor = Color.LightGreen;
+            }
+            if(timer == 20)
+            {
+                this.BackColor = Color.LightPink;
+            }
+            if(timer == 25)
+            {
+                timer = 0;
+            }
+        }
+    }
+}
